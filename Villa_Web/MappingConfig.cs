b6be9a0d176faf2f67ef5e571718d49d8c566c35
Nawon <nawon2794@gmail.com>
2025-09-30 23:52:24 +0530

    🔧 Add AutoMapper MappingConfig and register in services

---
+++ Villa_Web/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Villa_Web.Model.Dto;
+
+namespace Villa_Web
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap(); 
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+            CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+
+        }
+    }
+}
